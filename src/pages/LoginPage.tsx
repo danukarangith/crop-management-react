b4cd3a9616999d4705de84b86b85ae3fef1b3f63
danukarangith <danukarangith456@gmail.com>
2025-01-19 20:14:01 +0530

    sign in page create

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
+
 import '../css/Pages/LoginPage.css'
-import logo from '../assets/logo.png'
-import loginPageImg from '../assets/loginPageImg.png'
+import AgriNexus from '../assets/AgriNexus.png'
+import log2 from '../assets/log2.jpg'
 import {Link, useNavigate} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import {loginUser} from "../store/slices/userSlice.ts";
 import {toast} from "react-toastify";
@@ -15,6 +16,15 @@ const LoginPage = () => {
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
     const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
+    useEffect(() => {
+        const element = document.querySelector("body > div");
+
+        if (element && element instanceof HTMLElement) {
+            element.style.width = "75%";
+            element.style.height = "80%";
+        }
+    }, []);
 
     const togglePassword = () => {
         setIsPasswordVisible(!isPasswordVisible);
@@ -44,12 +54,12 @@ const LoginPage = () => {
             <div className="main-container d-grid w-100 overflow-hidden w-75 h-100">
                 <div className="p-5">
                     <div className="logo d-flex gap-4 align-items-center mt-5">
-                        <img src={logo} alt="logo"/>
-                        <h1 className="m-0 fw-medium">Green Shadow</h1>
+                        <img src={AgriNexus} alt="logo"/>
+                        <h1 className="m-0 fw-medium">Green Nexus</h1>
                     </div>
                     <div className="form-container mt-5">
                         <h3 className="fw-medium">Sign in your account</h3>
-                        <h4 className="mt-2 fw-medium">"Sign in to start managing your farm today!"</h4>
+                        <h4 className="mt-2 fw-medium">"Sign in Here!"</h4>
 
                         <div className="mt-5">
                             <div className="d-flex flex-column gap-3">
@@ -61,7 +71,7 @@ const LoginPage = () => {
                                         placeholder="name@example.com"
                                         onChange={(e) => setEmail(e.target.value)}
                                     />
-                                    <label htmlFor="floatingInput">Email address</label>
+                                    <label htmlFor="floatingInput">Enter your email here</label>
                                 </div>
                                 <div className="form-floating position-relative w-100 border-0">
                                     <input
@@ -71,7 +81,7 @@ const LoginPage = () => {
                                         placeholder="Password"
                                         onChange={(e) => setPassword(e.target.value)}
                                     />
-                                    <label htmlFor="floatingPassword">Password</label>
+                                    <label htmlFor="floatingPassword"> Enter your Password</label>
                                     <button
                                         type="button"
                                         className="btn btn-outline-secondary toggle-password bg-transparent border-0"
@@ -88,19 +98,19 @@ const LoginPage = () => {
                                 <label className="checkbox-btn d-block position-relative pointer-event p-3">
                                     <label className="pointer-event" htmlFor="checkbox"></label>
                                     <input id="checkbox" type="checkbox" className="position-absolute opacity-0 pointer-event"
-                                        onChange={() => setIsCheckboxChecked(!isCheckboxChecked)}
+                                           onChange={() => setIsCheckboxChecked(!isCheckboxChecked)}
                                     />
                                     <span className="checkmark position-absolute top-0 start-0"></span>
                                 </label>
                                 <h4 className="m-0 term fw-semibold">
-                                    I agree to all Term, Privacy Policy and Fees
+                                    I agree to all Term, Privacy Policy
                                 </h4>
                             </div>
                         </div>
                     </div>
                     <div className="d-flex align-items-center justify-content-between mt-5">
                         <button className="cssbuttons-io-button text-white fw-bold border-0 d-flex align-items-center overflow-hidden position-relative pointer-event"
-                            onClick={handleLogin}
+                                onClick={handleLogin}
                         >
                             Sign In
                             <div className="icon position-absolute d-flex align-items-center justify-content-center">
@@ -128,7 +138,7 @@ const LoginPage = () => {
                     <div className="h-100 d-flex align-items-center justify-content-start ms-5">
                         <canvas className="h-75"></canvas>
                     </div>
-                    <img className="w-100 h-100 mt-4 login-image" src={loginPageImg} alt=""/>
+                    <img className="w-100 h-100 mt-4 login-image" src={log2} alt=""/>
                 </div>
             </div>
         </>
